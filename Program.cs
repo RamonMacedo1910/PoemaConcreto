1dcdd1057d5917b3c31fa89a1b5a62d55ea1c7d7
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-22 23:53:10 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Um lindo poema concreto!");
+
+        Console.WriteLine();
+
+        Console.WriteLine("    UM");
+        Console.WriteLine("     MOVI");
+        Console.WriteLine("     MENTO");
+        Console.WriteLine("   COMPONDO");
+        Console.WriteLine("  ALÉM");
+        Console.WriteLine("         DA");
+        Console.WriteLine(" NUVEM");
+        Console.WriteLine("    UM");
+        Console.WriteLine("   CAMPO");
+        Console.WriteLine("      DE");
+        Console.WriteLine("   COMBATE");
+        Console.WriteLine("");
+        Console.WriteLine("     MIRA");
+        Console.WriteLine("   GEM");
+        Console.WriteLine("      IRA");
+        Console.WriteLine("         DE");
+        Console.WriteLine("    UM");
+        Console.WriteLine("      HORIZONTE");
+        Console.WriteLine("PURO");
+        Console.WriteLine("   NUM");
+        Console.WriteLine("     MO");
+        Console.WriteLine("     MENTO");
+        Console.WriteLine(" VIVO");
+        Console.WriteLine("");
+        Console.WriteLine("Décio Pignatari - 1956");
+        Console.WriteLine("");
